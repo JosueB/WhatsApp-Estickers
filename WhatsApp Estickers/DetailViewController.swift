54b02979173a7491945e41a1a3a83b992ff867aa
Josue_B <joshua593sk8@gmail.com>
2019-04-26 23:43:25 -0500

    Display 3 rows in collections view

--- WhatsApp Estickers/DetailViewController.swift
+++ WhatsApp Estickers/DetailViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class DetailViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
     @IBOutlet weak var mCollectionView: UICollectionView!
     
+    
+    let estickers: [EstickerItem] = [EstickerItem(imageUrl: "Barcelona"),
+                                     EstickerItem(imageUrl: "Barcelona"),
+                                     EstickerItem(imageUrl: "Barcelona"),
+                                     EstickerItem(imageUrl: "Barcelona"),
+                                     EstickerItem(imageUrl: "Barcelona"),
+                                     EstickerItem(imageUrl: "Barcelona"),
+                                     EstickerItem(imageUrl: "Barcelona")
+    ]
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return estickers.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "detailCell", for: indexPath) as! DetailViewCell
+        cell.mImage.image = UIImage(named: estickers[indexPath.row].imageUrl)
+        return cell
     }
     
+    
+    // MARK Methods that need to be over-written to display 3 rows in the collection view cell.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let yourWidth = collectionView.bounds.width/3.0
+        let yourHeight = yourWidth
+        
+        return CGSize(width: yourWidth, height: yourHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ WhatsApp Estickers/EstickerItem.swift
@@ -0,0 +1,11 @@
+//
+//  EstickerItem.swift
+//  WhatsApp Estickers
+//
+//  Created by Josue on 4/26/19.
+//  Copyright © 2019 Riley Norris. All rights reserved.
+//
+
+struct EstickerItem {
+    let imageUrl: String
+}
